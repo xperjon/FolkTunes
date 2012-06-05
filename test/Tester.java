701bab80e4f485a74eaa161d6e2c56488a6634f2
Jon-Erik Persson <jonerik.persson@gmail.com>
2012-06-05 21:11:13 +0200

    Added tester

---
+++ test/Tester.java
@@ -0,0 +1,23 @@
+
+import org.junit.AfterClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.BeforeClass;
+
+/**
+ *
+ * @author jep
+ */
+public class Tester {
+    
+    public Tester() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+}
